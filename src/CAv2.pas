744de62b66235f54f1ed77aee47c1328eeeefceb
Sun Serega <latchenko3@yandex.ru>
2018-11-06 14:42:08 +0200

    Added global try catch for v2

--- src/CAv2.pas
+++ src/CAv2.pas
@@ -112,130 +112,140 @@ begin
 end;
 
 begin
-  System.Console.SetWindowSize(Trunc(WR) * 2 + 1, Trunc(WR) * 2 + 2);
-  System.Console.SetWindowPosition(0, 0);
-  writeln('Press arrows to start');
-  
-  while true do
-  begin
-    var dx: shortint := 0;
-    var dy: shortint := 0;
-    if GetKeyState(37) div 128 = 1 then dx -= 1;
-    if GetKeyState(38) div 128 = 1 then dy -= 1;
-    if GetKeyState(39) div 128 = 1 then dx += 1;
-    if GetKeyState(40) div 128 = 1 then dy += 1;
+  try
+    System.Console.SetWindowSize(Trunc(WR) * 2 + 1, Trunc(WR) * 2 + 2);
+    System.Console.SetWindowPosition(0, 0);
+    writeln('Press arrows to start');
     
-    if (dx <> 0) or (dy <> 0) then
-      if IsOpen(dx, dy) then
-      begin
-        for i: integer := Map.Length - 1 downto 0 do
+    while true do
+    begin
+      var dx: shortint := 0;
+      var dy: shortint := 0;
+      if GetKeyState(37) div 128 = 1 then dx -= 1;
+      if GetKeyState(38) div 128 = 1 then dy -= 1;
+      if GetKeyState(39) div 128 = 1 then dx += 1;
+      if GetKeyState(40) div 128 = 1 then dy += 1;
+      
+      if (dx <> 0) or (dy <> 0) then
+        if IsOpen(dx, dy) then
         begin
-          Map[i].x -= dx;
-          Map[i].y -= dy;
-          
-          if sqr(Map[i].x) + sqr(Map[i].y) > sqr(MR) then
+          for i: integer := Map.Length - 1 downto 0 do
           begin
-            Map[i] := Map[Map.Length - 1];
-            SetLength(Map, Map.Length - 1);
-          end;
-        end;
-        
-        for n: byte := 1 to rnd do
-        begin
-          Map := Map + new Cave[1](new Cave(Random * MR * 2 - MR, Random * MR * 2 - MR, Random * MR / 2));
-          with Map[Map.Length - 1] do
-            if sqrt(sqr(x) + sqr(y)) < r + MR / 2 then
+            Map[i].x -= dx;
+            Map[i].y -= dy;
+            
+            if sqr(Map[i].x) + sqr(Map[i].y) > sqr(MR) then
+            begin
+              Map[i] := Map[Map.Length - 1];
               SetLength(Map, Map.Length - 1);
-        end;
-        
-        var nMob := Trunc(sqrt(Steps) / 10);
-        if Random < sqrt(Steps) / 10 - nMob then
-          nMob += 1;
-        
-        for i: integer := Mobs.Length - 1 downto 0 do
-          with Mobs[i] do
+            end;
+          end;
+          
+          for n: byte := 1 to rnd do
           begin
-            Mobs[i].x -= dx;
-            Mobs[i].y -= dy;
-            
-            var r := sqrt(sqr(x) + sqr(y));
-            var dr: real;
-            if IsOpen(x, y) then
-              dr := power(0.5, 100 / Steps) * 0.95 else
-              dr := power(0.1, 100 / Steps) * 0.95;
-            
-            dr *= sqrt(abs(dx) + abs(dy));
-            
-            for i2: integer := i + 1 to Mobs.Length - 1 do
-              if sqr(Mobs[i2].x - x) + sqr(Mobs[i2].x - x) < 1 then
-                dr -= 0.5;
-            
-            x *= (r - dr) / r;
-            y *= (r - dr) / r;
-            
-            if sqr(x) + sqr(y) < 2.25 then
+            Map := Map + new Cave[1](new Cave(Random * MR * 2 - MR, Random * MR * 2 - MR, Random * MR / 2));
+            with Map[Map.Length - 1] do
+              if sqrt(sqr(x) + sqr(y)) < r + MR / 2 then
+                SetLength(Map, Map.Length - 1);
+          end;
+          
+          var nMob := Trunc(sqrt(Steps) / 10);
+          if Random < sqrt(Steps) / 10 - nMob then
+            nMob += 1;
+          
+          for i: integer := Mobs.Length - 1 downto 0 do
+            with Mobs[i] do
             begin
-              if UseConsole then
-                System.Console.Clear;
-              var a := new byte[9, 8](
-              (0, 1, 1, 1, 1, 1, 1, 0),
-              (1, 1, 0, 0, 0, 0, 1, 1),
-              (1, 0, 0, 0, 0, 0, 0, 1),
-              (1, 0, 1, 0, 0, 1, 0, 1),
-              (1, 0, 0, 0, 0, 0, 0, 1),
-              (1, 0, 0, 1, 1, 0, 0, 1),
-              (1, 0, 1, 1, 1, 1, 0, 1),
-              (1, 1, 0, 0, 0, 0, 1, 1),
-              (0, 1, 1, 1, 1, 1, 1, 0));
-              for nx: integer := 0 to 8 do
+              Mobs[i].x -= dx;
+              Mobs[i].y -= dy;
+              
+              var r := sqrt(sqr(x) + sqr(y));
+              var dr: real;
+              if IsOpen(x, y) then
+                dr := power(0.5, 100 / Steps) * 0.95 else
+                dr := power(0.1, 100 / Steps) * 0.95;
+              
+              dr *= sqrt(abs(dx) + abs(dy));
+              
+              for i2: integer := i + 1 to Mobs.Length - 1 do
+                if sqr(Mobs[i2].x - x) + sqr(Mobs[i2].x - x) < 1 then
+                  dr -= 0.5;
+              
+              x *= (r - dr) / r;
+              y *= (r - dr) / r;
+              
+              if sqr(x) + sqr(y) < 2.25 then
               begin
-                for ny: integer := 0 to 7 do
+                if UseConsole then
+                  System.Console.Clear;
+                var a := new byte[9, 8](
+                (0, 1, 1, 1, 1, 1, 1, 0),
+                (1, 1, 0, 0, 0, 0, 1, 1),
+                (1, 0, 0, 0, 0, 0, 0, 1),
+                (1, 0, 1, 0, 0, 1, 0, 1),
+                (1, 0, 0, 0, 0, 0, 0, 1),
+                (1, 0, 0, 1, 1, 0, 0, 1),
+                (1, 0, 1, 1, 1, 1, 0, 1),
+                (1, 1, 0, 0, 0, 0, 1, 1),
+                (0, 1, 1, 1, 1, 1, 1, 0));
+                for nx: integer := 0 to 8 do
                 begin
-                  write(a[nx, ny] = 1 ? fill0 : fill3);
-                  sleep(10);
+                  for ny: integer := 0 to 7 do
+                  begin
+                    write(a[nx, ny] = 1 ? fill0 : fill3);
+                    sleep(10);
+                  end;
+                  writeln;
                 end;
-                writeln;
-              end;
-              Writeln($'You died with {Steps} steps');
-              Writeln('Press Enter to restart');
-              Writeln('Press Ecs to exit');
-              while true do
-              begin
-                if GetKeyState(13) div 128 = 1 then
+                Writeln($'You died with {Steps} steps');
+                Writeln('Press Enter to restart');
+                Writeln('Press Ecs to exit');
+                while true do
                 begin
-                  Map := new Cave[1](new Cave(0, 0, WR * sqrt(2)));
-                  Steps := -1;
-                  nMob := 0;
-                  Mobs := new Mob[0];
-                  break;
-                end else
-                if GetKeyState(27) div 128 = 1 then
-                  exit;
+                  if GetKeyState(13) div 128 = 1 then
+                  begin
+                    Map := new Cave[1](new Cave(0, 0, WR * sqrt(2)));
+                    Steps := -1;
+                    nMob := 0;
+                    Mobs := new Mob[0];
+                    break;
+                  end else
+                  if GetKeyState(27) div 128 = 1 then
+                    exit;
+                end;
+                break;
               end;
-              break;
+              
+              if sqr(x) + sqr(y) > sqr(MR) then
+              begin
+                Mobs[i] := Mobs[Mobs.Length - 1];
+                SetLength(Mobs, Mobs.Length - 1)
+              end;
             end;
-            
-            if sqr(x) + sqr(y) > sqr(MR) then
+          
+          for n: integer := 1 to nMob - Mobs.Length do
+            with Mobs[Mobs.Length] do
             begin
-              Mobs[i] := Mobs[Mobs.Length - 1];
-              SetLength(Mobs, Mobs.Length - 1)
+              Mobs := Mobs + new Mob[1];
+              while (sqr(x) + sqr(y) < sqr(MR / 2)) or (sqr(x) + sqr(y) > sqr(MR)) do
+              begin
+                x := (Random * 2 - 1) * MR;
+                y := (Random * 2 - 1) * MR;
+              end;
             end;
-          end;
-        
-        for n: integer := 1 to nMob - Mobs.Length do
-          with Mobs[Mobs.Length] do
-          begin
-            Mobs := Mobs + new Mob[1];
-            while (sqr(x) + sqr(y) < sqr(MR / 2)) or (sqr(x) + sqr(y) > sqr(MR)) do
-            begin
-              x := (Random * 2 - 1) * MR;
-              y := (Random * 2 - 1) * MR;
-            end;
-          end;
-        
-        Steps += 1;
-        Draw;
-        Sleep(Round(40 * sqrt(sqr(dx) + sqr(dy))));
-      end;
+          
+          Steps += 1;
+          Draw;
+          Sleep(Round(40 * sqrt(sqr(dx) + sqr(dy))));
+        end;
+    end;
+  except
+    on e: Exception do
+    begin
+      try System.Console.Clear except end;
+      writeln(e);
+      readln;
+      halt;
+    end;
   end;
 end.
